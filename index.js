f8e72c8d7365b1aafd869e9e7f856408769b9459
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-06-28 12:52:59 -0400

    commit tree 9b19476a0f2ebc6063405dc78a3134af72c5c0d6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity = "go to the office") {
+    return(`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(activity = "special") {
+        return(`You are ${visualFlair}${activity}${visualFlair}!`);
+    }
+}
+console.log(wrapAdjective());
